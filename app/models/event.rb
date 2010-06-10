4efa4f9430f462ce907fbe2b9b6277e7019a9501
Marko Peltola and Tero Hänninen <yousource-dev.group@korppi.jyu.fi>
2010-06-10 12:07:13 +0300

    Update vis code in Event: add method, make use of it in method latest
    Signed-off-by: Marko Peltola and Tero Hänninen <yousource-dev.group@korppi.jyu.fi>

--- app/models/event.rb
+++ app/models/event.rb
@@ -116,6 +116,11 @@ class Event < ActiveRecord::Base
     return false
   end
 
+  def visible?(logged_in)
+    return visibility_publics? if logged_in
+    return visibility_all?
+  end
+
   def self.latest(logged_in, count)
     Rails.cache.fetch("events:latest_#{count}", :expires_in => 10.minutes) do
       latest_event_ids = Event.find_by_sql(
@@ -125,11 +130,7 @@ class Event < ActiveRecord::Base
         ]).map(&:id)
       events = Event.find(latest_event_ids, :order => "created_at desc",
         :include => [:user, :project, :events])
-      if logged_in
-        events.delete_if { |e| !e.visibility_publics? }
-      else
-        events.delete_if { |e| !e.visibility_all?     }
-      end
+      events.delete_if { |e| !e.visible?(logged_in) }
     end
   end
 
