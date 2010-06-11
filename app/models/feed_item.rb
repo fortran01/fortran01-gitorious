a0323467fc8326277bd86065b9a2422ce72b26ac
Marko Peltola and Tero Hänninen <yousource-dev.group@korppi.jyu.fi>
2010-06-11 13:44:46 +0300

    FeedItem: dont create feeds of nonpublic events
    Signed-off-by: Marko Peltola and Tero Hänninen <yousource-dev.group@korppi.jyu.fi>

--- app/models/feed_item.rb
+++ app/models/feed_item.rb
@@ -21,6 +21,7 @@ class FeedItem < ActiveRecord::Base
   belongs_to :watcher, :class_name => "User"
 
   def self.bulk_create_from_watcher_list_and_event!(watcher_ids, event)
+    return unless event.visibility_all?
     return if watcher_ids.blank?
     # Build a FeedItem for all the watchers interested in the event
     sql_values = watcher_ids.map do |an_id|
