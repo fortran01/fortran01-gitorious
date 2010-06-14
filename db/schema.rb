6c70462c485d299511c4cb637131e2f2532e19bb
Marko Peltola and Tero Hänninen <yousource-dev.group@korppi.jyu.fi>
2010-06-14 12:23:36 +0300

    Update schema.rb after updating visibilities for projects and repos
    Signed-off-by: Marko Peltola and Tero Hänninen <yousource-dev.group@korppi.jyu.fi>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100609064319) do
+ActiveRecord::Schema.define(:version => 20100614091850) do
 
   create_table "archived_events", :force => true do |t|
     t.integer  "user_id"
